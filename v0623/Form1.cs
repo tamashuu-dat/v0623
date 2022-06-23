f4ae3534d9bd32ebe7bd2e9382ae54282d5b815b
A601-13 <tnk.dat.class@gmail.com>
2022-06-23 13:42:34 +0900

    復習

--- v0623/Form1.cs
+++ v0623/Form1.cs
@@ -12,10 +12,39 @@ namespace v0623
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+            if(label1.Left<0)
+            {
+                vx = -vx;
+            }
+            if(label1.Top<0)
+            {
+                vy = -vy;
+            }
+            if(label1.Right> ClientSize.Width)
+            {
+                vx = -vx;
+            }
+            if(label1.Bottom> ClientSize.Height)
+            {
+                vy = -vy;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show ="玉井秀兵";
+            timer1.Enabled = false;
+        }
     }
 }
